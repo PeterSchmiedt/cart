8f74e93dceae21da75b2234350fdd3f02e139487
Peter Schmiedt <schmipe4@fel.cvut.cz>
2018-03-13 16:04:55 +0100

    Initial Commit

---
+++ eman_cart/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  eman_cart
+//
+//  Created by Peter Schmiedt on 13/03/2018.
+//  Copyright © 2018 Peter Schmiedt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
